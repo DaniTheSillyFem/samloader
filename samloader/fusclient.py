9389fb5cfc4086140a3d118f8aff600a17c5d8f9
Duct <mariofatrio@gmail.com>
2026-05-19 13:09:39 +0200

    samloader: update FUS auth keys and session cookie
    Co-authored-by: henr1kas <161259379+henr1kas@users.noreply.github.com>

--- samloader/fusclient.py
+++ samloader/fusclient.py
@@ -20,7 +20,7 @@ class FUSClient:
         authv = 'FUS nonce="", signature="' + self.auth + '", nc="", type="", realm="", newauth="1"'
         req = requests.post("https://neofussvr.sslcs.cdngc.net/" + path, data=data,
                             headers={"Authorization": authv, "User-Agent": "Kies2.0_FUS"},
-                            cookies={"JSESSIONID": self.sessid})
+                            cookies={"SESSION": self.sessid})
         log_response(f"Request from FUSClient_MakeReq : \nAuthorization : {authv}\n Path : {path}\n Data Byte: {data}\n FinalRequest : {req.request.url} \n Headers : \n {req.request.headers}")
         # If a new NONCE is present, decrypt it and update our auth token.
         log_response(f"Nonce Request from {path}:\n{req.text}")
@@ -29,8 +29,8 @@ class FUSClient:
             self.nonce = auth.decryptnonce(self.encnonce)
             self.auth = auth.getauth(self.nonce)
         # Update the session cookie if needed.
-        if "JSESSIONID" in req.cookies:
-            self.sessid = req.cookies["JSESSIONID"]
+        if "SESSION" in req.cookies:
+            self.sessid = req.cookies["SESSION"]
         req.raise_for_status()
         return req.text
     def downloadfile(self, filename: str, start: int = 0) -> requests.Response:
